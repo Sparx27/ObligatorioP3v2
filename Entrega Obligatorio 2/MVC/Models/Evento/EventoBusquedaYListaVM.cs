20c54e0c18a48435f7d1d90b26079dd8fb5b318d
Cristian <cri_1112@hotmail.com>
2024-11-28 04:47:30 -0300

    Falta menos

--- Entrega Obligatorio 2/MVC/Models/Evento/EventoBusquedaYListaVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MVC.Models.Evento
-{
-    public class EventoBusquedaYListaVM
-    {
-        public EventoBusquedaVM BusquedaVM { get; set; } = new EventoBusquedaVM();
-        public IEnumerable<EventoListaVM>? ListaVM { get; set; }
-    }
-}
--- Entrega Obligatorio 2/MVC/Models/Usuario/UsuarioRolVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MVC.Models.Usuario
-{
-    public class UsuarioRolVM
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
